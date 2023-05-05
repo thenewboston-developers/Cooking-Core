f22092209842e16b7952ad37d2213da0489903ba
bucky <buckyroberts@gmail.com>
2023-05-05 17:15:38 -0400

    Update login response to include account data

--- cooking_core/authentication/views/login.py
+++ cooking_core/authentication/views/login.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from cooking_core.accounts.serializers.account import AccountSerializer
+
 from ..serializers.login import LoginSerializer
 
 
@@ -14,7 +16,9 @@ class LoginView(APIView):
         account = serializer.save()
         refresh_token = RefreshToken.for_user(account)
 
+        # TODO: Need to return account number as well
         return Response({
+            'account': AccountSerializer(account).data,
             'authentication': {
                 'access_token': str(refresh_token.access_token),
                 'refresh_token': str(refresh_token)
