09666484b8695f487e73c8041563f069b4c3f1bc
bucky <buckyroberts@gmail.com>
2023-05-05 13:56:03 -0400

    Cleaning

--- cooking_core/authentication/views/login.py
+++ cooking_core/authentication/views/login.py
@@ -1,11 +1,8 @@
-from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from ..serializers.login import LoginSerializer
-
-User = get_user_model()
 
 
 class LoginView(APIView):
@@ -14,8 +11,8 @@ class LoginView(APIView):
     def post(request):
         serializer = LoginSerializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        refresh_token = RefreshToken.for_user(user)
+        account = serializer.save()
+        refresh_token = RefreshToken.for_user(account)
 
         return Response({
             'authentication': {
