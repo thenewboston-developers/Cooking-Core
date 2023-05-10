5cc8ee6e21454dbf0e3c4ca81be65d896efeeaa0
bucky <buckyroberts@gmail.com>
2023-05-09 21:31:32 -0400

    Recipe

--- cooking_core/recipes/views/recipe.py
+++ cooking_core/recipes/views/recipe.py
@@ -19,5 +19,4 @@ class RecipeViewSet(viewsets.ModelViewSet):
         })
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
