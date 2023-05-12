0f818d22f0046813342eb5eb81a5607bf3bb6e41
bucky <buckyroberts@gmail.com>
2023-05-11 21:27:05 -0400

    Account update logic

--- cooking_core/accounts/views/account.py
+++ cooking_core/accounts/views/account.py
@@ -1,6 +1,7 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
+from cooking_core.general.permissions import IsAccountOwner
 from cooking_core.general.utils.cryptography import generate_key_pair
 
 from ..models import Account
@@ -21,3 +22,11 @@ class AccountViewSet(viewsets.ModelViewSet):
         }
 
         return Response(results, status=status.HTTP_201_CREATED)
+
+    def get_permissions(self):
+        if self.action == 'partial_update':
+            permission_classes = [IsAccountOwner]
+        else:
+            permission_classes = []
+
+        return [permission() for permission in permission_classes]
--- cooking_core/recipes/views/recipe.py
+++ cooking_core/recipes/views/recipe.py
@@ -1,15 +1,15 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
-from cooking_core.general.permissions import IsObjectCreator
+from cooking_core.general.permissions import IsObjectCreatorOrReadOnly
 
 from ..models import Recipe
 from ..serializers.recipe import RecipeSerializer
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsObjectCreatorOrReadOnly]
     queryset = Recipe.objects.all()
-    permission_classes = [IsObjectCreator]
     serializer_class = RecipeSerializer
 
     def create(self, request, *args, **kwargs):
@@ -19,4 +19,5 @@ class RecipeViewSet(viewsets.ModelViewSet):
         })
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+
         return Response(serializer.data, status=status.HTTP_201_CREATED)
