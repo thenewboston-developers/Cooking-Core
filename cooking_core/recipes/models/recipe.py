70e428c5d8205e3e45ef9fdab1ca3df15c1c00dd
bucky <buckyroberts@gmail.com>
2023-05-12 00:33:15 -0400

    Update recipe model to include image URL

--- cooking_core/recipes/models/recipe.py
+++ cooking_core/recipes/models/recipe.py
@@ -4,6 +4,7 @@ from django.db import models
 class Recipe(models.Model):
     creator = models.ForeignKey('accounts.Account', on_delete=models.CASCADE)
     description = models.TextField()
+    image_url = models.URLField()
     name = models.CharField(max_length=255)
 
     def __str__(self):
