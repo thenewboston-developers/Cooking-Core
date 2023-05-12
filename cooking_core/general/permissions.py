2962adf21e4939cb8b275cfa5c59bdcfa2e61f0a
Bucky Roberts <buckyroberts@gmail.com>
2023-05-11 21:28:00 -0400

    Account update logic (#16)

--- cooking_core/general/permissions.py
+++ cooking_core/general/permissions.py
@@ -1,7 +1,17 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework import permissions
 
 
-class IsObjectCreator(permissions.BasePermission):
+class IsAccountOwner(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if isinstance(request.user, AnonymousUser):
+            return False
+
+        return obj.account_number == request.user.account_number
+
+
+class IsObjectCreatorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
