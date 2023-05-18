4d68adc9beca0b61ff5893a519f8de305bd5b7ad
bucky <buckyroberts@gmail.com>
2023-05-18 15:10:15 -0400

    Get trusted origins from environment

--- cooking_core/project/settings/base.py
+++ cooking_core/project/settings/base.py
@@ -1,11 +1,19 @@
 from typing import List
 
+
+def get_trusted_origins():
+    try:
+        return CORE_TRUSTED_ORIGINS.split(',')  # type: ignore # noqa: F821
+    except NameError:
+        return []
+
+
 DEBUG = False
 SECRET_KEY = NotImplemented
 
 ALLOWED_HOSTS: List[str] = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS: List[str] = []
+CSRF_TRUSTED_ORIGINS: List[str] = get_trusted_origins()
 
 INTERNAL_IPS = [
     '127.0.0.1',
