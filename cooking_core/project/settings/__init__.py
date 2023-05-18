a32f5a68d0d5f616cdbf982ea04acd113dc46b92
bucky <buckyroberts@gmail.com>
2023-05-18 15:36:45 -0400

    Move settings ordering

--- cooking_core/project/settings/__init__.py
+++ cooking_core/project/settings/__init__.py
@@ -14,12 +14,12 @@ if not os.path.isabs(LOCAL_SETTINGS_PATH):
     LOCAL_SETTINGS_PATH = str(BASE_DIR / LOCAL_SETTINGS_PATH)
 
 include(
-    'envvars.py',
     'base.py',
     'logging.py',
     'rest_framework.py',
     'channels.py',
     'custom.py',
     optional(LOCAL_SETTINGS_PATH),
+    'envvars.py',
     'docker.py',
 )
