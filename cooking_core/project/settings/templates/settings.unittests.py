fa240f287f0be5cab0cc68fe742723bd9b8b7276
bucky <buckyroberts@gmail.com>
2023-05-25 20:16:02 -0400

    GitHub actions

--- cooking_core/project/settings/templates/settings.unittests.py
+++ cooking_core/project/settings/templates/settings.unittests.py
@@ -1,6 +1,5 @@
 DEBUG = True
 
-MIDDLEWARE += ('core.core.middleware.LoggingMiddleware',)  # type: ignore # noqa: F821
 LOGGING['formatters']['colored'] = {  # type: ignore # noqa: F821
     '()': 'colorlog.ColoredFormatter',
     'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
