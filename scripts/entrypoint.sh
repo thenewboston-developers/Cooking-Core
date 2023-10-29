0dde587c6eb9dc414b7d93ee4b3a4cd569b38ce1
maxXx <giulianopicco@gmail.com>
2023-10-29 13:53:40 -0300

    edit text to test qa workflow

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 RUN_MANAGE_PY='poetry run python -m cooking_core.manage'
 
-echo 'Collecting static files...'
+echo 'Collecting static files..'
 $RUN_MANAGE_PY collectstatic --no-input
 
 echo 'Running migrations...'
